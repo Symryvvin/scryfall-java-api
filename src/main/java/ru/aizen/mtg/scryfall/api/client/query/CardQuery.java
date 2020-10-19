e0d676c6b3f8bb7b841bcc1fba475e5d2eb4b33e
Aizen <symryvvin@gmail.com>
2020-10-19 15:58:25 +0300

    refactor: move method to build url in ScryfallQueryBuilder class

--- src/main/java/ru/aizen/mtg/scryfall/api/client/query/CardQuery.java
+++ src/main/java/ru/aizen/mtg/scryfall/api/client/query/CardQuery.java
@@ -1,11 +1,8 @@
 package ru.aizen.mtg.scryfall.api.client.query;
 
-import org.apache.http.client.utils.URIBuilder;
 import ru.aizen.mtg.scryfall.api.client.ScryfallClient;
 import ru.aizen.mtg.scryfall.api.domain.card.Card;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.UUID;
 
 public class CardQuery extends ScryfallQueryBuilder {
@@ -21,20 +18,8 @@ public class CardQuery extends ScryfallQueryBuilder {
 	 * @return a single card with the given Scryfall ID.
 	 */
 	public Card searchById(UUID id) throws ScryfallQueryException, ScryfallRequestException {
-		ScryfallRequest<Card> request = new ScryfallRequest<>(getClient(), generateMethod("/cards/" + id.toString()), Card.class);
+		ScryfallRequest<Card> request = new ScryfallRequest<>(getClient(), method("/cards/" + id.toString()), Card.class);
 		return request.execute();
-	}
-
-	private URI generateMethod(String path) throws ScryfallQueryException {
-		try {
-			URIBuilder builder = new URIBuilder();
-			builder.setScheme(getClient().getScheme());
-			builder.setHost(getClient().getEndpoint());
-			builder.setPath(path);
-			return builder.build();
-		} catch (URISyntaxException e) {
-			throw new ScryfallQueryException("Query building error", e);
-		}
 	}
 
 }
--- src/main/java/ru/aizen/mtg/scryfall/api/client/query/ScryfallQueryBuilder.java
+++ src/main/java/ru/aizen/mtg/scryfall/api/client/query/ScryfallQueryBuilder.java
@@ -1,6 +1,12 @@
 package ru.aizen.mtg.scryfall.api.client.query;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
 import ru.aizen.mtg.scryfall.api.client.ScryfallClient;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public abstract class ScryfallQueryBuilder {
 
@@ -12,6 +18,19 @@ public abstract class ScryfallQueryBuilder {
 
 	protected ScryfallClient getClient() {
 		return client;
+	}
+
+	protected URI method(String path, NameValuePair... params) throws ScryfallQueryException {
+		try {
+			URIBuilder builder = new URIBuilder();
+			builder.setScheme(getClient().getScheme());
+			builder.setHost(getClient().getEndpoint());
+			builder.setPath(path);
+			builder.addParameters(Arrays.asList(params));
+			return builder.build();
+		} catch (URISyntaxException e) {
+			throw new ScryfallQueryException("Query building error", e);
+		}
 	}
 
 }

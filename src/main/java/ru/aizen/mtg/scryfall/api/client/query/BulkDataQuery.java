acf89b06d7156a29aebf5287f73b1b9ae61d3260
Aizen <symryvvin@gmail.com>
2020-10-22 22:01:10 +0300

    refactor: BulkDataQuery now user superclass method to generate URI

--- src/main/java/ru/aizen/mtg/scryfall/api/client/query/BulkDataQuery.java
+++ src/main/java/ru/aizen/mtg/scryfall/api/client/query/BulkDataQuery.java
@@ -1,14 +1,11 @@
 package ru.aizen.mtg.scryfall.api.client.query;
 
-import org.apache.http.client.utils.URIBuilder;
 import ru.aizen.mtg.scryfall.api.client.ScryfallClient;
 import ru.aizen.mtg.scryfall.api.domain.bulk.BulkData;
 import ru.aizen.mtg.scryfall.api.domain.bulk.BulkDataList;
 import ru.aizen.mtg.scryfall.api.domain.bulk.BulkDataType;
 
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.UUID;
 
 public class BulkDataQuery extends ScryfallQueryBuilder {
@@ -23,7 +20,7 @@ public class BulkDataQuery extends ScryfallQueryBuilder {
 	 * @return a List of all Bulk Data items on Scryfall.
 	 */
 	public BulkDataList list() throws ScryfallQueryException, ScryfallRequestException {
-		ScryfallRequest<BulkDataList> request = new ScryfallRequest<>(getClient(), generateMethod("/bulk-data", false), BulkDataList.class);
+		ScryfallRequest<BulkDataList> request = new ScryfallRequest<>(getClient(), method("/bulk-data"), BulkDataList.class);
 		return request.execute();
 	}
 
@@ -34,7 +31,7 @@ public class BulkDataQuery extends ScryfallQueryBuilder {
 	 * @return a single Bulk Data object with the given type
 	 */
 	public BulkData byType(BulkDataType type) throws ScryfallQueryException, ScryfallRequestException {
-		ScryfallRequest<BulkData> request = new ScryfallRequest<>(getClient(), generateMethod("/bulk-data/" + type.getName(), false), BulkData.class);
+		ScryfallRequest<BulkData> request = new ScryfallRequest<>(getClient(), method("/bulk-data/" + type.getName()), BulkData.class);
 		return request.execute();
 	}
 
@@ -49,28 +46,12 @@ public class BulkDataQuery extends ScryfallQueryBuilder {
 	 * @return a single Bulk Data object with the given id (UUID).
 	 */
 	public BulkData byId(UUID id) throws ScryfallQueryException, ScryfallRequestException {
-		ScryfallRequest<BulkData> request = new ScryfallRequest<>(getClient(), generateMethod("bulk-data/" + id.toString(), false), BulkData.class);
+		ScryfallRequest<BulkData> request = new ScryfallRequest<>(getClient(), method("bulk-data/" + id.toString()), BulkData.class);
 		return request.execute();
 	}
 
 	public BulkData byIdAsFile(UUID id) {
 		throw new UnsupportedOperationException("Get bulk-data as file not supported");
 	}
-
-	private URI generateMethod(String path, boolean asFile) throws ScryfallQueryException {
-		try {
-			URIBuilder builder = new URIBuilder();
-			builder.setScheme(getClient().getScheme());
-			builder.setHost(getClient().getEndpoint());
-			builder.setPath(path);
-			if (asFile) {
-				builder.addParameter("format", "file");
-			}
-			return builder.build();
-		} catch (URISyntaxException e) {
-			throw new ScryfallQueryException("Query building error", e);
-		}
-	}
-
 
 }
